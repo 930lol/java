18b16c1df761ce845622695ef3b67fccb4186b2c
930lol <vikashsingh7209482955@gmail.com>
2020-12-29 18:10:38 +0530

    wap in java to count the Repeated character

---
+++ countrepeatedchar.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+public class Main
+{
+    public void find(String s)
+    {
+        Map<Character,Integer> baseMap=new HashMap<Character,Integer>();
+        char[] charArray=s.toCharArray();
+        for(Character ch:charArray)
+        {
+            if(baseMap.containsKey(ch))
+            {
+                baseMap.put(ch, baseMap.get(ch)+1);
+            }
+            else
+            {
+                baseMap.put(ch,1);
+            }
+        }
+        Set<Character> keys=baseMap.keySet();
+        for(Character ch:keys)
+        {
+            if(baseMap.get(ch)>1)
+            {
+                System.out.println(ch +" is " + baseMap.get(ch));
+            }
+        }
+    }
+    public static void main(String args[])
+    {
+        Main a=new Main();
+        a.find("India is my country");
+    }
+}
