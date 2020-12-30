ef544f83d890efff3e9dd6cabaf953948c57c1ca
930lol <vikashsingh7209482955@gmail.com>
2020-12-30 19:31:12 +0530

    wap in java to convert the character to integer format

---
+++ stringtoint.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String s="5";
+		int r=Integer.parseInt(s);
+		System.out.println(r);
+		String s2="5";
+		Integer r1=Integer.valueOf(s2);
+		System.out.println(r1);
+	}
+}
