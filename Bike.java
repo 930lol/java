48e0049093653d4a128cdb8aaa9cedde1b579c49
930lol <vikashsingh7209482955@gmail.com>
2021-02-24 23:56:23 +0530

    Inheritance child class of Vehicle

---
+++ Bike.java
@@ -0,0 +1,15 @@
+
+public class Bike extends Vehicle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bike b = new Bike();
+		b.start();
+		b.stop();
+		b.price=20222;
+		b.name="HERO HONDA";
+		System.out.println(b.price+"  "+b.name);
+
+	}
+
+}
