36b85197d6d6ae39c259dc15ece5aa1d6d558da8
930lol <vikashsingh7209482955@gmail.com>
2021-02-24 23:58:27 +0530

    Inheritance child class of Vegetable example

---
+++ Vegetable.java
@@ -0,0 +1,15 @@
+
+public class Vegetable {
+	int price;
+	String name;
+	int qty;
+	void washVeggi()
+	{
+		System.out.println("WASH THE VEGETABLE");
+	}
+	void cutVeggi()
+	{
+		System.out.println("CUT THE VEGETABLE");
+	}
+
+}
