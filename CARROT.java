05eff93047ab4768e2f314cd5c46cb92777e0799
930lol <vikashsingh7209482955@gmail.com>
2021-02-24 23:59:34 +0530

    Inheritance child class of Vegetable's child class

---
+++ CARROT.java
@@ -0,0 +1,15 @@
+
+public class CARROT extends Vegetable{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CARROT C =new CARROT();
+		C.washVeggi();
+		C.cutVeggi();
+		C.price=45;
+		C.name="Carrot";
+		C.qty=26;
+		System.out.println(C.price+" "+C.name+" "+C.qty);
+	}
+
+}
