cfafa6a230e30670a1d86c8c093a83ca4973ecc5
930lol <vikashsingh7209482955@gmail.com>
2021-02-25 23:59:02 +0530

    Inheritance child

---
+++ Father.java
@@ -0,0 +1,18 @@
+
+public class Father {
+	int money=100000;
+	String girlFriend="Kartina";
+	void walking()
+	{
+		System.out.println("WALKING IS GOOD FOR MENTAL HEALTH");
+	}
+	void drinking()
+	{
+		System.out.println("DRINKING IS BAD FOR MENTAL HEALTH");
+	}
+	void smoking()
+	{
+		System.out.println("SMOKING IS INGEROUS FOR MENTAL HEALTH");
+	}
+
+}
