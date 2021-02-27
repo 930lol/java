94a7a2d27499ebfaf80d5fd4377e50545dc2ebf1
930lol <vikashsingh7209482955@gmail.com>
2021-02-27 23:54:29 +0530

    Initilization of the instance variable using block

---
+++ Employee.java
@@ -0,0 +1,20 @@
+
+public class Employee {
+	//block 1
+	{
+		System.out.println("Block 1");
+	}
+	//block 2
+	{
+		System.out.println("Block 2");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee();
+		Employee e2 = new Employee();
+
+	}
+
+}
