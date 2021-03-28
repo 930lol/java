13539eeb20e7b2977b3414daa0e0687831f04b1e
930lol <vikashsingh7209482955@gmail.com>
2021-03-28 23:16:50 +0530

    ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY

---
+++ Pattern8.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Pattern8 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the row you want:");
+		int a=sc.nextInt();
+		int i,j;
+		char c=65;
+		for(i=1;i<=a;i++)
+		{
+			for(j=1;j<=a;j++)
+			{
+					System.out.print(c+" ");
+					c++;
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
