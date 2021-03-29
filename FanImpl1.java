0d7e8148bd740dd98c7f151c6b6a37ca35b67cc1
930lol <vikashsingh7209482955@gmail.com>
2021-03-29 23:37:28 +0530

    Interface fan method

---
+++ FanImpl1.java
@@ -0,0 +1,30 @@
+
+public class FanImpl1 implements Icombo{
+
+	@Override
+	public void increaseSpeed() {
+		// TODO Auto-generated method stub
+		System.out.println("Fan speed increase");
+		
+	}
+
+	@Override
+	public void reduceSpeed() {
+		// TODO Auto-generated method stub
+		System.out.println("Fan speed reduce");
+		
+	}
+
+	@Override
+	public void switchOn() {
+		// TODO Auto-generated method stub
+		System.out.println("Fan switch on");
+	}
+
+	@Override
+	public void switchOff() {
+		// TODO Auto-generated method stub
+		System.out.println("Fan switch off");
+		
+	}
+}
