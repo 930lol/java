f9e1b6d4ca3d4ee93793431fe3a7e5a7c5442548
930lol <vikashsingh7209482955@gmail.com>
2021-03-29 23:43:40 +0530

    Test function

---
+++ Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Icombo s= new FanImpl1();
+		s.switchOff();
+		s.switchOn();
+		s.reduceSpeed();
+		s.increaseSpeed();
+
+	}
+
+}
