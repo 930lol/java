5216a1ca1bf1614c5274227dc24e7dc6ff251b19
930lol <vikashsingh7209482955@gmail.com>
2021-03-30 12:17:57 +0530

    1      \n    0 1 0    \n   1 0 1 0 1  \n 0 1 0 1 0 1 0 \n1 0 1 0 1 0 1 0 1 0 1

---
+++ Pattern27.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Pattern27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of lines you want");
+		int n=sc.nextInt();
+		int i,j,k=1;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n-i;j++)
+			{
+				System.out.print("  ");
+			}
+			for(j=1;j<=2*i-1;j++)
+			{
+				if(i%2!=0)
+					if(j%2!=0)
+					{
+						System.out.print(1+" ");
+					}
+					else
+					{
+						System.out.print(0+" ");
+					}
+				else
+					if(j%2!=0)
+					{
+						System.out.print(0+" ");
+					}
+					else
+					{
+						System.out.print(1+" ");
+					}		
+			}
+
+			System.out.println();
+		}
+	}
+}
+
