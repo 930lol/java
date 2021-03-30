ad7bce8c0c0e3bbac9dd8296302e33eb72b49645
930lol <vikashsingh7209482955@gmail.com>
2021-03-30 12:16:17 +0530

    1      \n    1 0 1    \n   1 0 1 0 1  \n1 0 1 0 1 0 1 0 1 \n1 0 1 0 1 0 1 0 1 0 1

---
+++ Pattern26.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pattern26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of lines you want");
+		int n=sc.nextInt();
+		int i,j,k=1;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n-i;j++)
+			{
+				System.out.print("  ");
+			}
+			for(j=1;j<=2*i-1;j++)
+			{
+				if(j%2==0)
+					System.out.print("0 ");
+				else
+					System.out.print("1 ");
+			}
+
+			System.out.println();
+		}
+	}
+}
+
