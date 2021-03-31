1533a61d6bea48ffefe8e23f877e8cbe08bace1e
930lol <vikashsingh7209482955@gmail.com>
2021-03-31 23:17:51 +0530

    A     \n    A B A   \n   A B C B A  \n A B C D C B A \ A B C D E D C B A

---
+++ Pattern34.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Pattern34 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of lines you want");
+		int n=sc.nextInt();
+		int i,j;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n-i;j++)
+			{
+				System.out.print("  ");
+			}
+			char a=65;
+			for(j=1;j<=2*i-1;j++)
+			{
+				System.out.print(a+" ");
+				if(j<i)
+				{
+					a++;
+				}
+				else
+				{
+					a--;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+
