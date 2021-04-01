76b698f717e2de5d09103872e02f990d685d52c8
930lol <vikashsingh7209482955@gmail.com>
2021-04-01 22:36:43 +0530

    hollow diamond

---
+++ Pattern43.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+public class Pattern43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number of");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,j;
+		int sp=n/2,st=1;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=sp;j++)
+			{
+				System.out.print("  ");
+			}
+			for(j=1;j<=st;j++)
+			{
+				if(j==1||j==st)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			if(i<=n/2)
+			{
+				sp--;
+				st=st+2;
+			}
+			else
+			{
+				sp++;
+				st=st-2;
+			}
+			System.out.println();
+
+	}
+	}
+}
