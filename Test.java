8140e0e04ecf12f9705beacc3874136aa6187628
930lol <vikashsingh7209482955@gmail.com>
2021-04-03 22:06:11 +0530

    Consumer or Utilization logic

--- Test.java
+++ Test.java
@@ -1,12 +1,20 @@
-
-public class Test {
-
-	public static void main(String[] args) {
- 
-		IMouse m= new CPUImpl();
-		m.click();
-		m.doubleClick();
-		m.rightClick();
+package org.btm.mobileApp;
+import java.util.Scanner;
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Mobile name");
+		String type=sc.next();
+		sc.close();
+		ITouch it = MoblieFactory.getMobile(type);
+		if(it!=null)
+		{
+			it.text();
+			it.call();
+			it.playMusic();
+		}
 	}
 
 }
