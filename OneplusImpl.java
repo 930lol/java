d8363b6047afbb85618c97a928e6319ff06468d7
930lol <vikashsingh7209482955@gmail.com>
2021-04-03 22:08:05 +0530

    Implemantation logic

---
+++ OneplusImpl.java
@@ -0,0 +1,22 @@
+package org.btm.mobileApp;
+
+public class OneplusImpl implements ITouch
+{
+	@Override
+	public void text() 
+	{
+		System.out.println("Texting on oneplus !!!");
+	}
+	@Override
+	public void call() 
+	{
+		System.out.println("Calling on oneplus !!!");
+	}
+	@Override
+	public void playMusic() 
+	{
+		System.out.println("Playing music on oneplus !!!");
+		
+	}
+
+}
