93c3fffb9af78d894fbba1ac8f6cfbdbd66181ff
930lol <vikashsingh7209482955@gmail.com>
2021-04-19 21:35:45 +0530

    Matrix Test

--- Test1.java
+++ Test1.java
@@ -1,9 +1,23 @@
-
-public class Test1 {
-
-	public static void main(String[] args) {
-		VendingMachine vm = new VendingMachine();
-		Baverage b= vm.pressButton();
-		System.out.println(b);
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		MatrixColBiggestReturnArray m =  new MatrixColBiggestReturnArray(); 
+		System.out.println("Read the matrix:");
+		int a[][]=m.readMat();
+		System.out.println("Display the elements of the matrix:");
+		m.dispMat(a);
+		System.out.println("Col wise biggest elements:");
+		int[] c=m.colWiseBiggest(a);
+		for(int i=0;i<c.length;i++)
+		{
+			System.out.print(c[i]);
+			if(i<c.length-1)
+			{
+				System.out.print(",");
+			}
+		}
+		
 	}
+
 }
